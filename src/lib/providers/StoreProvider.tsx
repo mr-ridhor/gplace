b828ddcadffb3114efb17a23a212f71ab10f3b75
mr-ridhor <surajudeen.ridwan.a@gmail.com>
2024-09-07 16:31:12 +0100

    spome

--- src/lib/providers/StoreProvider.tsx
+++ src/lib/providers/StoreProvider.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "../services/storeService";
-// import { persistor, store } from "../services/storeService";
 
 export const StoreProvider = ({ children }: { children: React.ReactNode }) => {
   return (
