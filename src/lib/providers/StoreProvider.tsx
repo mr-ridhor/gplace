1c892f0f0421153abfeaef0e012e66aabc8de961
mr-ridhor <surajudeen.ridwan.a@gmail.com>
2024-09-07 18:41:03 +0100

    revsed

--- src/lib/providers/StoreProvider.tsx
+++ src/lib/providers/StoreProvider.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "../services/StoreService";
+import { persistor, store } from "../services/storeService";
 // import { persistor, store } from "../services/storeService";
 // import { persistor, store } from "../services/storeService";
 
