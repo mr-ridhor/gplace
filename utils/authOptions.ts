3a11688602e8921c74ae497aeccb3b28bfd787f1
Emie-Trialer <emiedonmukumo@gmail.com>
2024-09-07 21:00:45 +0100

    updated the authOption

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -4,11 +4,6 @@ import connectDB from "../config/db";
 import User from "../models/User";
 import { NextAuthOptions } from "next-auth";
 
-interface CredentialsType {
-    email: string;
-    password: string;
-}
-
 interface UserType {
     _id: string;
     name: string;
@@ -32,27 +27,40 @@ export const authOptions: NextAuthOptions = {
                     type: 'text'
                 },
             },
-            authorize: async (credentials: CredentialsType) => {
+            authorize: async (credentials, req) => {
+                if (!credentials) {
+                    return null;
+                }
+
+                const { email, password } = credentials;
+
                 await connectDB();
 
-                const user = await User.findOne({ 'credentials.email': credentials.email }) as UserType | null;
+                // Fetch the user by email
+                const user = (await User.findOne({
+                    "credentials.email": email,
+                })) as UserType | null;
                 if (!user) {
                     throw new Error("No user found");
                 }
 
-                // Compare the provided password with the stored hashed password using bcryptjs
-                const isPasswordValid = await verifyPassword(credentials.password, user.credentials.password);
+                // Compare the password with the stored hashed password
+                const isPasswordValid = await verifyPassword(
+                    password,
+                    user.credentials.password
+                );
+
                 if (!isPasswordValid) {
                     throw new Error("Invalid password");
                 }
 
-                // If the password is valid, return the user object
+                // Return the user object including the required jwt field
                 return {
                     id: user._id,
                     name: user.name,
-                    email: user.email,
+                    email: user.email
                 };
-            }
+            },
         })
     ],
     pages: {
