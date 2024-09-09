1cf26feef9232580bf73faed92c1097a0037b246
Emie-Trialer <emiedonmukumo@gmail.com>
2024-09-09 18:36:32 +0100

    added homepage

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='p-5'>
+      <h1 className="text-2xl font-bold">Welcome to Goodplace CRM</h1>
+    </div>
+  )
+}
+
+export default page
