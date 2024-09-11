92efaebdd99217a25ce1319deb2d9def33db70be
mr-ridhor <surajudeen.ridwan.a@gmail.com>
2024-09-11 16:36:03 +0100

    new

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,34 @@
-// import { withAuth } from "next-auth/middleware";
-
-// export default withAuth(
-//   function middleware(req) {
-//     // Custom logic if needed
-//   },
-//   {
-//     callbacks: {
-//       authorized({ token, req }) {
-//         const { pathname } = req.nextUrl;
-
-//         // Corrected the condition to group it properly with parentheses
-//         if (
-//           pathname.startsWith("/auth/register") ||
-//           pathname.startsWith("/auth/forgett-password")
-//         ) {
-//           return true;
-//         }
-
-//         // Standard authentication check
-//         return !!token;
-//       },
-//     },
-//     pages: {
-//       signIn: "/auth/login", // Redirect to login if not authenticated
-//       error: "/error", // Error page
-//     },
-//   }
-// );
 import { withAuth } from "next-auth/middleware";
-export default withAuth(function middleware(req) {
-  console.log("here", req.nextauth.token);
-});
-export const config = { matcher: ["/dashboard"] };
+
+export default withAuth(
+  function middleware(req) {
+    // Custom logic if needed
+  },
+  {
+    callbacks: {
+      authorized({ token, req }) {
+        const { pathname } = req.nextUrl;
+
+        // Corrected the condition to group it properly with parentheses
+        if (
+          pathname.startsWith("/auth/register") ||
+          pathname.startsWith("/auth/forgett-password")
+        ) {
+          return true;
+        }
+
+        // Standard authentication check
+        return !!token;
+      },
+    },
+    pages: {
+      signIn: "/auth/login", // Redirect to login if not authenticated
+      error: "/error", // Error page
+    },
+  }
+);
+// import { withAuth } from "next-auth/middleware";
+// export default withAuth(function middleware(req) {
+//   console.log("here", req.nextauth.token);
+// });
+// export const config = { matcher: ["/dashboard"] };
