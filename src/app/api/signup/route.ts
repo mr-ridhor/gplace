54ef309cea747256a0d14fba7c59fde2bec77dfb
mr-ridhor <surajudeen.ridwan.a@gmail.com>
2024-09-12 13:44:49 +0100

    agga

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -3,46 +3,46 @@ import User from "../../../../models/User";
 import transporter from "../../../../utils/transporter"; // Adjust the path if needed
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const { bio, company, team, credentials } = await req.json();
-    console.log(await req.json());
-    // await connectDB();
+
+    await connectDB();
 
     // Generate a verification code
-    // const verificationCode = Math.floor(10000 + Math.random() * 90000);
+    const verificationCode = Math.floor(10000 + Math.random() * 90000);
 
     // Add the verification code to credentials before creating the user
-    // credentials.verificationCode = verificationCode;
+    credentials.verificationCode = verificationCode;
 
     // Create a new user with the verification code included
-    // const newUser = await User.create({
-    //   bio,
-    //   company,
-    //   team,
-    //   credentials,
-    // });
-    // console.log(newUser);
+    const newUser = await User.create({
+      bio,
+      company,
+      team,
+      credentials,
+    });
+
     // Email options for verification code
-    // const mailOptions = {
-    //   from: process.env.ADMIN_MAIL, // Sender address
-    //   to: newUser.credentials.email, // Recipient email
-    //   subject: "Your Verification Code", // Subject line
-    //   text: `Your verification code is: ${verificationCode}`, // Plain text body
-    // };
+    const mailOptions = {
+      from: process.env.ADMIN_MAIL, // Sender address
+      to: newUser.credentials.email, // Recipient email
+      subject: "Your Verification Code", // Subject line
+      text: `Your verification code is: ${verificationCode}`, // Plain text body
+    };
 
-    // // Send the verification email
-    // transporter.sendMail(mailOptions, (error: any, info: any) => {
-    //   if (error) {
-    //     console.error("Error sending email:", error);
-    //     return NextResponse.json(
-    //       { message: "Error sending email", error: error.message },
-    //       { status: 500 }
-    //     );
-    //   }
+    // Send the verification email
+    transporter.sendMail(mailOptions, (error: any, info: any) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        return NextResponse.json(
+          { message: "Error sending email", error: error.message },
+          { status: 500 }
+        );
+      }
 
-    //   console.log(info);
-    // });
+      console.log(info);
+    });
 
     return NextResponse.json(
       { message: "User created successfully and verification code sent" },
