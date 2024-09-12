0a34cd965e9b069d843d079ead5e87b2a5cc6c03
Emie-Trialer <emiedonmukumo@gmail.com>
2024-09-12 16:33:51 +0100

    added getUser in utils folder

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,40 @@
-'use client'
-// import { getServerSession } from 'next-auth'
+// 'use client'
 import React from 'react'
 import { signOut } from 'next-auth/react'
-// import { authOptions } from '../../utils/authOptions'
 import axios from 'axios'
+import { getUser } from '../../utils/getUser'
 
-const page = () => {
+const page = async () => {
+  const handleTest = async () => {
+    // const response = await axios.get('/api/investors')
+
+    // const response = axios.post('/api/signup', payload)
+    // const response = await axios.post('/api/investors', payload) // this is for posting new investor
+    // const response = await axios.get('/api/investors') //this is for getting all investors,
+    // const response = await axios.get('/api/investors/66e187858f191cc51aebfce9/contact') // to get or post
+
+    // const response = await axios.get('/api/investors/66e187858f191cc51aebfce9') // to get a single investor
+    // const response = await axios.put('/api/investors/66e187858f191cc51aebfce9', payload) // to update a single investor
+    // const response = await axios.delete('/api/investors/66e187858f191cc51aebfce9') // to delete a single investor
+    if (response.status == 200) {
+      console.log(await response.data)
+    }
+  }
+
+  const user = await getUser()
+
+  return (
+    <div className='p-5'>
+      <h1 className="text-2xl font-bold">Welcome to Goodplace CRM</h1>
+      <h1 className="text-2xl">{user.firstName} {user.lastName}</h1>
+      {/* <button onClick={handleTest} className=''>Test API route</button> */}
+      {/* <button className='mx-8 p-2 border' onClick={()=> signOut({ callbackUrl: '/auth/login' })}>Signout</button> */}
+    </div>
+  )
+}
+
+export default page
+
   // getting the user data such as firstName, lastName, and email
   // const data = await getServerSession(authOptions)
   // console.log(data)
@@ -16,6 +45,58 @@ const page = () => {
   //     "email": "emiedonmokumo@volariscapital.com",
   //   }
   // }
+  
+
+//  signup payload
+  // const payload = {
+  //   "bio": {
+  //     "firstName": "Emie", // (required)
+  //     "lastName": "Boro", // (required)
+  //     "title": "Software Engineer", // (required)
+  //     "email": "emieboro8@gmail.com", // (required)
+  //     "linkedIn": "https://www.linkedin.com/in/emieboro8", // (optional)
+  //     "X": "@emieboro8", // (optional)
+  //     "country": "USA", // (required)
+  //     "city": "New York", // (required)
+  //     "address": "123 Main St, New York, NY 10001" // (required)
+  //   },
+  //   "company": {
+  //     "name": "Whitebeard Inc.", // (required)
+  //     "country": "USA", // (required)
+  //     "city": "New York", // (required)
+  //     "email": "info@whitebeard.com", // (required)
+  //     "website": "https://www.whitebeard.com", // (optional)
+  //     "industry": "Technology", // (required)
+  //     "foundingYear": 2010, // (required)
+  //     "revenue": {
+  //       "ltm": 5000000, // (required) Last Twelve Months Revenue
+  //       "previousYear": 4500000 // (required)
+  //     },
+  //     "grossProfit": {
+  //       "ltm": 2000000, // (required)
+  //       "previousYear": 1800000 // (required)
+  //     },
+  //     "EBITDA": {
+  //       "ltm": 1000000, // (required) Last Twelve Months EBITDA
+  //       "previousYear": 900000 // (required)
+  //     }
+  //   },
+  //   "team": {
+  //     "team1": {
+  //       "fullName": "Alice Johnson", // (optional)
+  //       "role": "Product Manager" // (optional)
+  //     },
+  //     "team2": {
+  //       "fullName": "Bob Smith", // (optional)
+  //       "role": "Lead Developer" // (optional)
+  //     }
+  //   },
+  //   "credentials": {
+  //     "email": "emieboro8@gmail.com", // (required)
+  //     "password": "12345678" // (required)
+  //   }
+  // }
+
 
   // this is for post request
 
@@ -76,80 +157,3 @@ const page = () => {
   //   "vertical": "Software",
   //   "status": "Data Exchange",
   // }
-
-  //  signup payload
-  // const payload = {
-  //   "bio": {
-  //     "firstName": "Emie", // (required)
-  //     "lastName": "Boro", // (required)
-  //     "title": "Software Engineer", // (required)
-  //     "email": "emieboro8@gmail.com", // (required)
-  //     "linkedIn": "https://www.linkedin.com/in/emieboro8", // (optional)
-  //     "X": "@emieboro8", // (optional)
-  //     "country": "USA", // (required)
-  //     "city": "New York", // (required)
-  //     "address": "123 Main St, New York, NY 10001" // (required)
-  //   },
-  //   "company": {
-  //     "name": "Whitebeard Inc.", // (required)
-  //     "country": "USA", // (required)
-  //     "city": "New York", // (required)
-  //     "email": "info@whitebeard.com", // (required)
-  //     "website": "https://www.whitebeard.com", // (optional)
-  //     "industry": "Technology", // (required)
-  //     "foundingYear": 2010, // (required)
-  //     "revenue": {
-  //       "ltm": 5000000, // (required) Last Twelve Months Revenue
-  //       "previousYear": 4500000 // (required)
-  //     },
-  //     "grossProfit": {
-  //       "ltm": 2000000, // (required)
-  //       "previousYear": 1800000 // (required)
-  //     },
-  //     "EBITDA": {
-  //       "ltm": 1000000, // (required) Last Twelve Months EBITDA
-  //       "previousYear": 900000 // (required)
-  //     }
-  //   },
-  //   "team": {
-  //     "team1": {
-  //       "fullName": "Alice Johnson", // (optional)
-  //       "role": "Product Manager" // (optional)
-  //     },
-  //     "team2": {
-  //       "fullName": "Bob Smith", // (optional)
-  //       "role": "Lead Developer" // (optional)
-  //     }
-  //   },
-  //   "credentials": {
-  //     "email": "emieboro8@gmail.com", // (required)
-  //     "password": "12345678" // (required)
-  //   }
-  // }
-
-  const handleTest = async () => {
-    // const response = axios.post('/api/signup', payload)
-    // const response = await axios.post('/api/investors', payload) // this is for posting new investor
-    // const response = await axios.get('/api/investors') //this is for getting all investors,
-    const response = await axios.get('/api/investors/66e187858f191cc51aebfce9/contact') // to get or post
-
-    // const response = await axios.get('/api/investors/66e187858f191cc51aebfce9') // to get a single investor
-    // const response = await axios.put('/api/investors/66e187858f191cc51aebfce9', payload) // to update a single investor
-    // const response = await axios.delete('/api/investors/66e187858f191cc51aebfce9') // to delete a single investor
-    if (response.status == 200) {
-      console.log(await response.data)
-    }
-  }
-
-  return (
-    <div className='p-5'>
-      <h1 className="text-2xl font-bold">Welcome to Goodplace CRM</h1>
-      {/* <h1 className="text-2xl">{data.user.id}</h1> */}
-      {/* <h1 className="text-2xl">{new Date(data.exp).toLocaleString()}</h1> */}
-      <button onClick={handleTest} className=''>Test API route</button>
-      <button className='mx-8 p-2 border' onClick={()=> signOut({ callbackUrl: '/auth/login' })}>Signout</button>
-    </div>
-  )
-}
-
-export default page
---
+++ utils/getUser.ts
@@ -0,0 +1,7 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "./authOptions"
+
+export const getUser = async () =>{
+    const data = await getServerSession(authOptions)
+    return data?.user
+}
