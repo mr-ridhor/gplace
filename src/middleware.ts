00a90430ef901e5b1e86a4ddf3f08da44c32c57e
Emie-Trialer <emiedonmukumo@gmail.com>
2024-09-12 12:29:36 +0100

    Updated the signup and login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,37 @@
-// import { withAuth } from "next-auth/middleware";
-export { default } from 'next-auth/middleware'
+import { withAuth } from "next-auth/middleware";
+// export { default } from 'next-auth/middleware'
 
-export const config = {
-  matcher: ['/dashboard']
-}
+// export const config = {
+//   matcher: ['/dashboard']
+// }
 
-// export default withAuth(
-//   function middleware(req) {
-//     // Custom logic if needed
-//   },
-//   {
-//     callbacks: {
-//       authorized({ token, req }) {
-//         const { pathname } = req.nextUrl;
+export default withAuth(
+  function middleware(req) {
+    // Custom logic if needed
+  },
+  {
+    callbacks: {
+      authorized({ token, req }) {
+        const { pathname } = req.nextUrl;
 
-//         // Corrected the condition to group it properly with parentheses
-//         if (
-//           pathname.startsWith("/auth/register") ||
-//           pathname.startsWith("/auth/forget-password")
-//         ) {
-//           return true;
-//         }
+        // Corrected the condition to group it properly with parentheses
+        if (
+          pathname.startsWith("/auth/register") ||
+          pathname.startsWith("/auth/forget-password")
+        ) {
+          return true;
+        }
 
-//         // Standard authentication check
-//         return !!token;
-//       },
-//     },
-//     pages: {
-//       signIn: "/auth/login", // Redirect to login if not authenticated
-//       error: "/error", // Error page
-//     },
-//   }
-// );
+        // Standard authentication check
+        return !!token;
+      },
+    },
+    pages: {
+      signIn: "/auth/login", // Redirect to login if not authenticated
+      error: "/error", // Error page
+    },
+  }
+);
 
 // import { withAuth } from "next-auth/middleware";
 
