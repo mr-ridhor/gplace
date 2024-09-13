4704f13d3893af076850bf0f211f50c945200674
mr-ridhor <surajudeen.ridwan.a@gmail.com>
2024-09-13 10:32:05 +0100

    type error fixed

--- src/app/api/email/otp/route.ts
+++ src/app/api/email/otp/route.ts
@@ -5,64 +5,64 @@ import mg from "../../../../../utils/mailgun";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: NextRequest) {
-    try {
-        const { email, userId } = await req.json();
+  try {
+    const { email, userId } = await req.json();
 
-        await connectDB();
+    await connectDB();
 
-        // Generate a verification code
-        const user = await User.findOne({ 'credentials.email': email, _id: userId });
-        if (!user) {
-            return NextResponse.json(
-                { message: "User not found" },
-                { status: 404 }
-            );
-        }
-
-        const verificationCode = Math.floor(10000 + Math.random() * 90000);
-
-        // Assign the verification code and save the user
-        user.credentials.verificationCode = verificationCode;
-        await user.save();
-
-        // Email options for verification code
-        // const mailOptions = {
-        //     from: process.env.ADMIN_MAIL, // Sender address
-        //     to: user.credentials.email, // Recipient email
-        //     subject: "Your Verification Code", // Subject line
-        //     text: `Your verification code is: ${verificationCode}`, // Plain text body
-        // };
-
-        // Send the verification email
-        // transporter.sendMail(mailOptions, (error: any, info: any) => {
-        //     if (error) {
-        //         console.error("Error sending email:", error);
-        //         return NextResponse.json(
-        //             { message: "Error sending email", error: error.message },
-        //             { status: 500 }
-        //         );
-        //     }
-
-        //     console.log("Email sent:", info);
-        // });
-
-
-        mg.messages.create('sandbox011c4bce2719422796e9147ba253b308.mailgun.org', {
-            from: "GoodPlace CRM <mailgun@sandbox011c4bce2719422796e9147ba253b308.mailgun.org>",
-            to: user.credentials.email,
-            subject: "Verification Code",
-            text: "Testing some Mailgun awesomness!",
-            html: `<h1>Your verification code is: ${verificationCode}</h1>`
-        })
-            .then(msg => console.log(msg)) // logs response data
-            .catch(err => console.error(err));
-
-        return NextResponse.json(
-            { message: "Verification code sent" },
-            { status: 201 }
-        );
-    } catch (error: any) {
-        console.error("Error:", error);
-        return NextResponse.json({ message: error.message }, { status: 500 });
+    // Generate a verification code
+    const user = await User.findOne({
+      "credentials.email": email,
+      _id: userId,
+    });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
+
+    const verificationCode = Math.floor(10000 + Math.random() * 90000);
+
+    // Assign the verification code and save the user
+    user.credentials.verificationCode = verificationCode;
+    await user.save();
+
+    // Email options for verification code
+    // const mailOptions = {
+    //     from: process.env.ADMIN_MAIL, // Sender address
+    //     to: user.credentials.email, // Recipient email
+    //     subject: "Your Verification Code", // Subject line
+    //     text: `Your verification code is: ${verificationCode}`, // Plain text body
+    // };
+
+    // Send the verification email
+    // transporter.sendMail(mailOptions, (error: any, info: any) => {
+    //     if (error) {
+    //         console.error("Error sending email:", error);
+    //         return NextResponse.json(
+    //             { message: "Error sending email", error: error.message },
+    //             { status: 500 }
+    //         );
+    //     }
+
+    //     console.log("Email sent:", info);
+    // });
+
+    mg.messages
+      .create("sandbox011c4bce2719422796e9147ba253b308.mailgun.org", {
+        from: "GoodPlace CRM <mailgun@sandbox011c4bce2719422796e9147ba253b308.mailgun.org>",
+        to: user.credentials.email,
+        subject: "Verification Code",
+        text: "Testing some Mailgun awesomness!",
+        html: `<h1>Your verification code is: ${verificationCode}</h1>`,
+      })
+      .then((msg: any) => console.log(msg)) // logs response data
+      .catch((err: any) => console.error(err));
+
+    return NextResponse.json(
+      { message: "Verification code sent" },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    console.error("Error:", error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
