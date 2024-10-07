c4c111990ea7e2a1a2e6e516c9cf272c443845e1
Emie-Trialer <emiedonmukumo@gmail.com>
2024-10-07 18:46:12 +0100

    update

--- src/app/crm/page.jsx
+++ src/app/crm/page.jsx
@@ -1,22 +1,10 @@
 'use client'
 import React from 'react'
-import axios from 'axios'
 
 const page = () => {
     const handleClick = async () =>{
-        // const contactObj = {
-        //     properties: {
-        //       firstname: 'Emiedonmokumo',
-        //       lastname: 'Dick-Boro',
-        //       email: 'emieboro8@gmail.com',
-        //     },
-        // }
+
     
-        const response = await axios.get('https://api.hubapi.com/crm/v3/properties/contacts', {
-            headers: {
-                Authorization: 'Bearer pat-na1-de50b3f8-817a-4de5-95cf-a1084908c475'
-            }
-        });
         const json = await response.data
         console.log(json)
     }
