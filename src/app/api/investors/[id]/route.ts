8f2b22e269dc210a5406b62c35dd3d3dd030b3d0
Emie-Trialer <emiedonmukumo@gmail.com>
2024-10-13 15:04:02 +0100

    update PUT Investors

--- src/app/api/investors/[id]/route.ts
+++ src/app/api/investors/[id]/route.ts
@@ -4,6 +4,7 @@ import { authOptions } from "../../../../../utils/authOptions";
 import { NextRequest, NextResponse } from "next/server";
 import Investor from "../../../../../models/Investor";
 import InvestorContact from "../../../../../models/InvestorContact";
+import User, { IUser } from "../../../../../models/User";
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
     try {
@@ -29,6 +30,7 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
 };
 
 
+
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
     try {
         await connectDB();
@@ -38,17 +40,36 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
             return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
         }
 
-        const updateData = await req.json();
+        const { companyInfo, investmentBio, targetInfo, offeredPriceValuation, paidInfo } = await req.json();
+        const investor = await Investor.findOne({ _id: params.id, user: data.user.id })
+        const user: IUser | any = await User.findById(data.user.id)
 
-        const updatedInvestor = await Investor.findOneAndUpdate(
+        if (!investor) return NextResponse.json({ message: 'Investor not found' }, { status: 404 });
+
+        const updatedCompanyInfo = companyInfo ? { ...investor.companyInfo, ...companyInfo } : investor.companyInfo;
+        const updatedInvestmentBio = investmentBio ? { ...investor.investmentBio, ...investmentBio } : investor.investmentBio;
+        const updatedTargetInfo = targetInfo ? { ...investor.targetInfo, ...targetInfo } : investor.targetInfo;
+        const updatedPaidInfo = paidInfo ? { ...investor.paidInfo, ...paidInfo } : investor.paidInfo;
+        const updatedOfferedPrice = offeredPriceValuation ? {
+            ...investor.offeredPrice, valuation: offeredPriceValuation,
+            revenue: parseFloat((offeredPriceValuation / user.company.revenue.ltm).toFixed(1)),
+            EBITDA: parseFloat((offeredPriceValuation / user.company.EBITDA.ltm).toFixed(1))
+        } : investor.targetInfo;
+
+
+        await Investor.findOneAndUpdate(
             { user: data.user.id, _id: params.id },
-            { $set: updateData }, // Only update fields provided in updateData
+            {
+                $set: {
+                    companyInfo: updatedCompanyInfo,
+                    investmentBio: updatedInvestmentBio,
+                    targetInfo: updatedTargetInfo,
+                    paidInfo: updatedPaidInfo,
+                    offeredPrice: updatedOfferedPrice
+                }
+            }, // Only update fields provided in updateData
             { new: true }
         );
-
-        if (!updatedInvestor) {
-            return NextResponse.json({ message: 'Investor not found' }, { status: 404 });
-        }
 
         return NextResponse.json({ message: 'Investor Data Updated Successfully' }, { status: 200 });
     } catch (error: any) {
@@ -56,6 +77,8 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
         return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
+
+
 
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
     try {
@@ -72,7 +95,7 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
         if (!investor) {
             return NextResponse.json({ message: 'Investor not found' }, { status: 404 });
         }
-        if(investorContact.length !== 0) await InvestorContact.deleteMany({ investor: investor._id })
+        if (investorContact.length !== 0) await InvestorContact.deleteMany({ investor: investor._id })
         await Investor.deleteOne({ user: data.user.id, _id: params.id })
         return NextResponse.json({ message: 'Investor Data Deleted Successfully' }, { status: 200 });
     } catch (error: any) {
