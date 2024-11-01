363045d03185fc4ebc4c7e75b994661d721f8783
Emie-Trialer <emiedonmukumo@gmail.com>
2024-11-01 05:55:45 +0100

    GET note/[id]

--- src/app/api/investors/[id]/note/[noteId]/route.ts
+++ src/app/api/investors/[id]/note/[noteId]/route.ts
@@ -13,3 +13,13 @@ export async function DELETE(req: NextRequest, { params }: { params: { noteId: s
     await note.deleteOne()
     return NextResponse.json({ message: 'Note Deleted Successfully' }, { status: 200 })
 }
+
+export async function GET(req: NextRequest, { params }: { params: { noteId: string } }) {
+    const user = await getUser()
+    await connectDB()
+
+    const note = await Note.findOne({ _id: params.noteId, user: user?.id })
+    if (!note) return NextResponse.json({ message: 'Note not found' }, { status: 404 })
+
+    return NextResponse.json(note, { status: 200 })
+}
