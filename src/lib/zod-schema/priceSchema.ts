b7f79310a0ad659b2e85cb7fba1cec11f7793959
mr-ridhor <surajudeen.ridwan.a@gmail.com>
2024-11-15 08:37:08 +0100

    all

--- src/lib/zod-schema/priceSchema.ts
+++ src/lib/zod-schema/priceSchema.ts
@@ -17,15 +17,15 @@ import { z } from "zod";
 // });
 export const priceSchema = z.object({
 	val: z.object({
-		from: z.string().regex(/^\d+(\.\d+)?$/, "Must be a valid number"),
-		to: z.string().regex(/^\d+(\.\d+)?$/, "Must be a valid number"),
+		from: z.string().regex(/^\d+(\.\d+)?$/, "Must be a  number"),
+		to: z.string().regex(/^\d+(\.\d+)?$/, "Must be a  number"),
 	}),
 	evRev: z.object({
-		from: z.string().regex(/^\d+(\.\d+)?$/, "Must be a valid number"),
-		to: z.string().regex(/^\d+(\.\d+)?$/, "Must be a valid number"),
+		from: z.string().regex(/^\d+(\.\d+)?$/, "Must be a  number"),
+		to: z.string().regex(/^\d+(\.\d+)?$/, "Must be a  number"),
 	}),
 	evEbd: z.object({
-		from: z.string().regex(/^\d+(\.\d+)?$/, "Must be a valid number"),
-		to: z.string().regex(/^\d+(\.\d+)?$/, "Must be a valid number"),
+		from: z.string().regex(/^\d+(\.\d+)?$/, "Must be a  number"),
+		to: z.string().regex(/^\d+(\.\d+)?$/, "Must be a  number"),
 	}),
 });
